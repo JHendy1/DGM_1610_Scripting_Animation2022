3133191ace3cb7f31551ec72b177c103bdd65ab6
john henderson <john.h602@stu.nebo.edu>
2022-09-29 09:20:17 -0600

    Make player move

--- Rogue Guy/Assets/Scripts/PlayerController.cs
+++ Rogue Guy/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    private float hInput;
+    private float vInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right * hInput * speed * Time.deltaTime);
+        //Move left to right
+
+        transform.Translate(Vector2.up * vInput * speed * Time.deltaTime);
+        //Move up and down
+
     }
 }
