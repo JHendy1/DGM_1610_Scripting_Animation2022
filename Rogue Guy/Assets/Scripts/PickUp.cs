71274719bb62b92652efa583df432b10173d6bea
john henderson <john.h602@stu.nebo.edu>
2022-10-05 09:21:08 -0600

    created pickup for key

---
+++ Rogue Guy/Assets/Scripts/PickUp.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public string name;
+    public string typeOfPickup;
+    public string description;
+    
+    public int amount;
+    public int value;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
