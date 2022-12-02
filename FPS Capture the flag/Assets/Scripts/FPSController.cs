1fcbeaeabf73244e8d2d62708b7026f0a4153102
john henderson <john.h602@stu.nebo.edu>
2022-12-02 09:13:42 -0700

    pick up script

--- FPS Capture the flag/Assets/Scripts/FPSController.cs
+++ FPS Capture the flag/Assets/Scripts/FPSController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,15 +12,17 @@ public class FPSController : MonoBehaviour
     public float lookSensitivity;
     public float maxLookX;
     public float minLookX;
-    public float rotX;
+    private float rotX;
     [Header("Private Variables")]
     private Camera camera;
     private Rigidbody rb;
     void Awake()
     {
-        //Get Components
+        //Get Components 
         camera = Camera.main;
         rb = GetComponent<Rigidbody>();
+        //freeze cursor
+        Cursor.lockState = CursorLockMode.Locked;
     }
     // Start is called before the first frame update
     void Start()
@@ -30,18 +33,53 @@ public class FPSController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMove();
+        CameraLook();
+        //jump
+        if (Input.GetButtonDown("Jump"))
+            PlayerJump();
     }
+
+  
+
     void PlayerMove()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed; //move left and right
         float z = Input.GetAxis("Vertical") * moveSpeed; //move backward and forward
-        rb.velocity = new Vector3(x, rb.velocity.y, z); // applies velocity on x and y axis
+
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir.y = rb.velocity.y;
+
+        rb.velocity = dir; // applies velocity on x and y axis
     }
 
     void CameraLook()
     {
-        float y = Input.GetAxis("Mouse x") * lookSensitivity;
-        rotX += Input.GetAxis("Mouse y") * lookSensitivity;
+        float y = Input.GetAxis("Mouse X") * lookSensitivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+        camera.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
+    }
+
+    void PlayerJump()
+    {
+        //ground detection
+        Ray ray = new Ray(transform.position, Vector3.down);
+        //jump ability
+        if (Physics.Raycast(ray, 1.1f))
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+    }
+
+    public void GiveHealth(int amount)
+    {
+
+    }
+
+    public void GiveAmmo(int amount)
+    {
+
     }
 }
---
+++ FPS Capture the flag/Assets/Scripts/PickUp.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PickupType
+{
+    Health,
+    Ammo
+}
+
+public class PickUp : MonoBehaviour
+{
+    [Header("Pickup Stats")]
+    public PickupType type;
+    public int healthAmount;
+    public int ammoAmount;
+
+    [Header("Bobbing Anim")]
+    public float rotationSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+
+    private Vector3 startPos;
+    private bool bobbingUp;
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set start position
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            FPSController player = other.GetComponent<FPSController>();
+
+            switch(type)
+            {
+                case PickupType.Health:
+                    player.GiveHealth(healthAmount);
+                    break;
+
+                case PickupType.Ammo:
+                    player.GiveAmmo(ammoAmount);
+                    break;
+
+            }
+
+            //play pickup audio clip
+            //other.Getcomponent<AudioSurce>().PlayOneShot(pickupSfx
+
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotating
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+        //Bob up and down
+        Vector3 offset = (bobbingUp == true ? new Vector3(0, bobHeight / 2, 0) : new Vector3(0, -bobHeight / 2, 0));
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+
+        if (transform.position == startPos + offset)
+            bobbingUp = !bobbingUp;
+    }
+}
